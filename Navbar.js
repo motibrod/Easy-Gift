df6992369b3a6d518e3423944ae1eb815ab923b3
motibrod <95278103+motibrod@users.noreply.github.com>
2022-11-06 10:41:51 +0200

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -1,129 +1,34 @@
-import React, { useState } from "react";
-import { Box, Button, TextField, Typography, ButtonGroup } from "@mui/material";
+import React from "react";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
-const Details = () => {
-  const [inputs, setInput] = useState({
-    name: "",
-    phone: "",
-    blessing: "",
-  });
-  const handelchange = (e) => {
-    setInput((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-  console.log(inputs);
+const Navbar = () => {
   return (
-    <div>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "left",
-          "& > *": {
-            m: 1,
-          },
-        }}
-      >
-        <ButtonGroup variant="text" aria-label="text button group">
-          <Button>עיברית</Button>
-          <Button>English</Button>
-        </ButtonGroup>
-      </Box>
-      <form>
-        <Box
-          display="flex"
-          flexDirection={"column"}
-          maxWidth={600}
-          alignItems="center"
-          justifyContent={"center"}
-          margin="auto"
-          marginTop={5}
-          padding={3}
-          borderRadius={5}
-          boxShadow={"5px 5px 10px #ccc"}
-          sx={{
-            ":hover": {
-              boxShadow: "10px 10px 20px #ccc",
-              "& button": { m: 1 },
-            },
-          }}
-        >
-          <Typography variant="h2" padding={3} textAlign="center">
-            Eazy Gift
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
           </Typography>
-          <Typography
-            variant="h4"
-            padding={3}
-            textAlign="center"
-          >
-            Johnny and Lily's wedding
-          </Typography>
-
-          <Box
-            component="form"
-            sx={{
-              "& > :not(style)": { m: 1, width: "25ch" },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <TextField
-              onChange={handelchange}
-              name="name"
-              value={inputs.name}
-              id="outlined-basic"
-              label="name"
-              variant="outlined"
-            />
-          </Box>
-
-          <Box
-            component="form"
-            sx={{
-              "& > :not(style)": { m: 1, width: "25ch" },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <TextField
-              onChange={handelchange}
-              name="phone"
-              value={inputs.phone}
-              id="outlined-basic"
-              label="phone"
-              variant="outlined"
-            />
-          </Box>
-
-          <Box
-            component="form"
-            sx={{
-              "& > :not(style)": { m: 1, width: "25ch" },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <TextField
-              onChange={handelchange}
-              name="blessing"
-              value={inputs.blessing}
-              id="outlined-basic"
-              label="blessing"
-              variant="outlined"
-              multiline
-              rows={4}
-            />
-          </Box>
-          <Button size="small">List of written blessing</Button>
-          <Button variant="contained" size="large">
-            payment
-          </Button>
-        </Box>
-      </form>
-    </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
-export default Details;
+export default Navbar;
